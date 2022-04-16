dca2aaca0d0408a2170ee381d39e35495e160a33
ymp-a <hiyakashi3@gmail.com>
2022-04-16 23:59:03 +0900

    tempAppを追加

---
+++ tempApp/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  tempApp
+//
+//  Created by satoshi yamashita on 2022/03/17.
+//
+
+import SwiftUI
+
+enum Fruits: String, CaseIterable, Identifiable {
+    var id: String { self.rawValue }
+    case apple = "りんご"
+    case banana = "ばなな"
+    case grape = "ぶどう"
+}
+
+struct ContentView: View {
+    // enum型を宣言＆初期化している
+    @State var fruit: Fruits = .apple
+
+    var body: some View {
+        // ピッカー時にも必須？
+        VStack {
+            // "picker名",selection:enum変数を指定
+            Picker("Select Fruit", selection: $fruit) {
+                // enum内の全要素分繰り返す
+                // id: \.selfの部分で.appleとかが初期化されてる気がする
+                ForEach(Fruits.allCases, id: \.self) { value in
+                    Text(value.rawValue)
+                        // picker選択できるようにタグ付け
+                        .tag(value)
+                }
+            }
+            // ホイールピッカースタイル
+            .pickerStyle(WheelPickerStyle())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tempApp/tempAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempAppApp.swift
+//  tempApp
+//
+//  Created by satoshi yamashita on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct tempAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
