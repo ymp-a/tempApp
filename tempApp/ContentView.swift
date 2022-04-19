638b4e11368a4a9e5520dd826a1423b7a7c11090
ymp-a <hiyakashi3@gmail.com>
2022-04-19 21:12:08 +0900

    円錐式グラデーションボタン

--- tempApp/ContentView.swift
+++ tempApp/ContentView.swift
@@ -7,38 +7,83 @@
 
 import SwiftUI
 
-// 列挙型を試した
-enum Fruits: String, CaseIterable, Identifiable {
-    var id: String { self.rawValue }
-    case apple = "りんご"
-    case banana = "ばなな"
-    case grape = "ぶどう"
-}
-
+// グラデーションボタンと最前面への設置検証
 struct ContentView: View {
-    // enum型を宣言＆初期化している
-    @State var fruit: Fruits = .apple
+    // 参照先 https://blog.personal-factory.com/2020/05/04/customize-navigationbar-in-ios13/
+    // メモ一覧部分を白背景にする
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.white
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    var body: some View {
+        ZStack {
+            VStack {
+                NavigationView {
+                    List {
+                    }
+                    .navigationTitle("メモ一覧")
+                    .navigationBarTitleDisplayMode(.automatic)
+                } // NavigationViewここまで
+            } // VStack
+            Text("なし")
+                .font(.title)
+            FloatingButton()
+        } // ZStack
+    }
+} // struct MemoListsViewここまで
+
+// https://dev.classmethod.jp/articles/swiftui_floatingbutton_linkage_textfield/
+// https://capibara1969.com/1800/
+// ボタンの外観
+struct FloatingButton: View {
+    let gradientView = AngularGradient(
+        // 円錐式グラデーション
+        gradient: Gradient(colors: [Color(UIColor.blue), Color(UIColor.green)]),
+        center: .center,
+        angle: .degrees(0))
 
     var body: some View {
-        // ピッカー時にも必須？
-        VStack {
-            // "picker名",selection:enum変数を指定
-            Picker("Select Fruit", selection: $fruit) {
-                // enum内の全要素分繰り返す
-                // id: \.selfの部分で.appleとかが初期化されてる気がする
-                ForEach(Fruits.allCases, id: \.self) { value in
-                    Text(value.rawValue)
-                        // picker選択できるようにタグ付け
-                        .tag(value)
-                }
-            }
-            // ホイールピッカースタイル
-            .pickerStyle(WheelPickerStyle())
-        }
-    }
-}
+        VStack {  // --- 1
+            Spacer()
+            HStack { // --- 2
+                Spacer()
+                Button(action: {
+                    print("Tapped!!") // --- 3
+                }, label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24)) // --- 4
+                })
+                .frame(width: 60, height: 60)
+                .background(gradientView)
+                .cornerRadius(30.0)
+                .shadow(color: .gray, radius: 3, x: 3, y: 3)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 16.0, trailing: 16.0)) // --- 5
 
-struct ContentView_Previews: PreviewProvider {
+            } // HStackここまで
+        } // VStackここまで
+    } // bodyここまで
+    //    /// 背景グラデーションを作成する
+    //    private func backGroundColor() -> LinearGradient {
+    //        // 左上から右下にポイントを設定する。
+    //        let start = UnitPoint.init(x: 1, y: 1) // 左上(始点)
+    //        let end = UnitPoint.init(x: 0.5, y: 0) // 右下(終点)
+    //
+    //        // 「Color」は以前の「UIColor」からの変換もできるぞ！ 助かる。
+    //        let colors = Gradient(colors: [Color(UIColor.blue), Color(UIColor.green)])
+    //
+    //        let gradientColor = LinearGradient(gradient: colors, startPoint: start, endPoint: end)
+    //
+    //        return gradientColor
+    //    }
+} // FlontingButtonここまで
+
+struct MemoListsView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
